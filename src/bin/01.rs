2ba9361868c5f8aedd3d6f44d7f8e8a6aeadee2d
Matt Armstrong <matt@rfc20.org>
2023-09-09 10:47:16 -0700

    Day 01 part two

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,4 +1,4 @@
-pub fn parse_input(input: &str) -> Vec<i32> {
+pub(crate) fn parse_input(input: &str) -> Vec<u32> {
     input
         .split_whitespace()
         .map(|s| s.parse().unwrap())
@@ -6,13 +6,24 @@ pub fn parse_input(input: &str) -> Vec<i32> {
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
-    let numbers = parse_input(input);
-    let sum: u32 = numbers.iter().map(|&num| (num / 3 - 2) as u32).sum();
+    let input = parse_input(input);
+    let sum = input.iter().map(|&num| (num / 3 - 2)).sum();
     Some(sum)
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+pub fn fuel_part_two(input: u32) -> u32 {
+    let fuel: u32 = input / 3 - 2;
+    if fuel <= 5 {
+        fuel
+    } else {
+        fuel + fuel_part_two(fuel)
+    }
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let input = parse_input(input);
+    let sum = input.iter().map(|&num| fuel_part_two(num)).sum();
+    Some(sum)
 }
 
 fn main() {
