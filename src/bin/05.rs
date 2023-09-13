e9deae6f322ab1b9f8aca0cc5931967c8d62c6e6
Matt Armstrong <matt@rfc20.org>
2023-09-13 10:06:40 -0700

    Day 05 part 2

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -6,15 +6,21 @@ enum Opcode {
     Multiply,
     Input,
     Output,
+    JumpIfTrue,
+    JumpIfFalse,
+    LessThan,
+    Equals,
     Finished,
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 struct Computer {
     pc: i32,
     memory: Vec<i32>,
+    input: i32,
     output: Vec<i32>,
     finished: bool,
+    trace: bool,
 }
 
 impl Computer {
@@ -22,13 +28,17 @@ impl Computer {
         Computer {
             pc: 0,
             memory: Vec::new(),
+            input: 0,
             output: Vec::new(),
             finished: false,
+            trace: false,
         }
     }
 
     fn get(&self, address: i32) -> i32 {
-        // println!("    data[{}] -> {}", address, self.data[address as usize]);
+        if self.trace {
+            println!("    memory[{}] -> {}", address, self.memory[address as usize]);
+        }
         self.memory[address as usize]
     }
 
@@ -40,22 +50,34 @@ impl Computer {
             2 => Opcode::Multiply,
             3 => Opcode::Input,
             4 => Opcode::Output,
+            5 => Opcode::JumpIfTrue,
+            6 => Opcode::JumpIfFalse,
+            7 => Opcode::LessThan,
+            8 => Opcode::Equals,
             99 => Opcode::Finished,
             _ => panic!("Invalid instruction: {} at pc {}", instruction, self.pc),
         }
     }
 
     fn store(&mut self, param: i32, value: i32) {
-        // println!("  store: param={} value={}", param, value);
+        if self.trace {
+            println!("  store: param={} value={}", param, value);
+        }
         let address = self.get(self.pc + param) as usize;
-        // println!("    {} -> data[{}]", value, address);
+        if self.trace {
+            println!("    {} -> memory[{}]", value, address);
+        }
         self.memory[address] = value
     }
 
     fn load(&self, param: i32) -> i32 {
-        // println!("  load: param={}", param);
+        if self.trace {
+            println!("  load: param={}", param);
+        }
         let instruction = self.get(self.pc);
-        // println!("    instruction: {}", instruction);
+        if self.trace {
+            println!("    instruction: {}", instruction);
+        }
         assert!(instruction >= 0);
 
         let mut tmp = instruction / 100;
@@ -64,7 +86,9 @@ impl Computer {
         }
         let immediate = tmp % 10 == 1;
 
-        // println!("    immediate: {}", immediate);
+        if self.trace {
+            println!("    immediate: {}", immediate);
+        }
         let value = self.get(self.pc + param);
         if immediate {
             value
@@ -74,75 +98,138 @@ impl Computer {
     }
 
     fn step(&mut self) {
-        // println!("step: pc={}", self.pc);
-        // let mut debug_data = &self.data[(self.pc as usize)..];
-        // if debug_data.len() > 4 {
-        //     debug_data = &debug_data[0..4];
-        // }
-        // println!("  program at pc: {:?}", debug_data);
+        if self.trace {
+            println!("step: pc={}", self.pc);
+            let mut debug_memory = &self.memory[(self.pc as usize)..];
+            if debug_memory.len() > 4 {
+                debug_memory = &debug_memory[0..4];
+            }
+            println!("  program at pc: {:?}", debug_memory);
+        }
 
         match self.opcode() {
             Opcode::Add => {
-                // println!("  add");
+                if self.trace {
+                    println!("  add");
+                }
                 self.store(3, self.load(1) + self.load(2));
                 self.pc += 4
             }
             Opcode::Multiply => {
-                // println!("  multiply");
+                if self.trace {
+                    println!("  multiply");
+                }
                 self.store(3, self.load(1) * self.load(2));
                 self.pc += 4
             }
             Opcode::Input => {
-                // println!("  input");
-                self.store(1, 1);
+                if self.trace {
+                    println!("  input");
+                }
+                self.store(1, self.input);
                 self.pc += 2
             }
             Opcode::Output => {
-                // println!("  output");
+                if self.trace {
+                    println!("  output");
+                }
                 let value = self.load(1);
-                // println!("    value: {}", value);
+                if self.trace {
+                    println!("    value: {}", value);
+                }
                 self.output.push(value);
                 self.pc += 2;
-                if self.memory[self.pc as usize] != 99 {
-                    assert_eq!(value, 0);
+            }
+            Opcode::JumpIfTrue => {
+                if self.trace {
+                    println!("  jump-if-true");
+                }
+                let value = self.load(1);
+                self.pc = if value != 0 {
+                    self.load(2)
+                } else {
+                    self.pc + 3
                 }
             }
+            Opcode::JumpIfFalse => {
+                if self.trace {
+                    println!("  jump-if-false");
+                }
+                let value = self.load(1);
+                self.pc = if value == 0 {
+                    self.load(2)
+                } else {
+                    self.pc + 3
+                }
+            }
+            Opcode::LessThan => {
+                if self.trace {
+                    println!("  less-than");
+                }
+                let less = self.load(1) < self.load(2);
+                if self.trace {
+                    println!("    less: {}", less);
+                }
+                self.store(3, if less { 1 } else { 0 });
+                self.pc += 4
+            }
+            Opcode::Equals => {
+                if self.trace {
+                    println!("  equals");
+                }
+                let equals = self.load(1) == self.load(2);
+                if self.trace {
+                    println!("    equals: {}", equals);
+                }
+                self.store(3, if equals { 1 } else { 0 });
+                self.pc += 4
+            }
             Opcode::Finished => {
-                // println!("  finished");
+                if self.trace {
+                    println!("  finished");
+                }
                 self.finished = true;
             }
         }
     }
 
     fn run(&mut self) {
-        // let debug_data = &self.data[..].get(..16).unwrap_or(&self.data);
-        // println!("run: data={:?}...", debug_data);
-
         while !self.finished {
             self.step();
         }
     }
 }
 
-fn parse_computer_string(string: &str) -> Computer {
+fn parse_program(text: &str) -> Computer {
     let mut computer = Computer::new();
-    for number in string.trim_end().split(',') {
+    for number in text.trim_end().split(',') {
         computer.memory.push(number.parse().unwrap_or_else(|e| {
             panic!("Invalid numerical string: \"{}\" error: {}", number, e);
         }));
     }
+    computer.input = 1; // the default for part one
     computer
 }
 
-pub fn part_one(input: &str) -> Option<u32> {
-    let mut computer = parse_computer_string(input);
+pub fn run_program(program_text: &str, input_number: i32, trace: bool) -> Vec<i32> {
+    let mut computer = parse_program(program_text);
+    computer.input = input_number;
+    computer.trace= trace;
+    if trace {
+        println!("run_program: {:?}", computer);
+    }
     computer.run();
-    // println!("output: {:?}", computer.output);
-    computer.output.last().map(|x| *x as u32)
+    computer.output
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+pub fn part_one(input: &str) -> Option<u32> {
+    let output = run_program(input, 1, false);
+    output.last().map(|x| *x as u32)
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let output = run_program(input, 5, false);
+    output.last().map(|x| *x as u32)
 }
 
 fn main() {
@@ -157,7 +244,7 @@ mod tests {
 
     #[test]
     fn test_step_add() {
-        let mut computer = parse_computer_string("1,5,6,7,99,11,13,0");
+        let mut computer = parse_program("1,5,6,7,99,11,13,0");
         computer.step();
         assert_eq!(computer.pc, 4);
         assert_eq!(computer.memory, vec![1, 5, 6, 7, 99, 11, 13, 11 + 13]);
@@ -165,7 +252,7 @@ mod tests {
 
     #[test]
     fn test_step_multiply() {
-        let mut computer = parse_computer_string("2,5,6,7,99,11,13,10000");
+        let mut computer = parse_program("2,5,6,7,99,11,13,10000");
         computer.step();
         assert_eq!(computer.pc, 4);
         assert_eq!(computer.memory, vec![2, 5, 6, 7, 99, 11, 13, 11 * 13]);
@@ -173,7 +260,7 @@ mod tests {
 
     #[test]
     fn test_part_one() {
-        let mut computer = parse_computer_string("1,9,10,3,2,3,11,0,99,30,40,50");
+        let mut computer = parse_program("1,9,10,3,2,3,11,0,99,30,40,50");
         computer.step();
         assert_eq!(computer.pc, 4);
         assert_eq!(
@@ -188,12 +275,13 @@ mod tests {
             vec![3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50]
         );
 
+        computer.step();
         assert!(computer.finished);
     }
 
     #[test]
     fn test_output_example() {
-        let mut computer = parse_computer_string("3,0,4,0,99");
+        let mut computer = parse_program("3,0,4,0,99");
         computer.step();
         assert_eq!(computer.pc, 2);
         assert_eq!(computer.memory, vec![1, 0, 4, 0, 99]);
@@ -204,9 +292,85 @@ mod tests {
         assert_eq!(computer.memory, vec![1, 0, 4, 0, 99]);
         assert_eq!(computer.output, vec![1]);
 
+        computer.step();
         assert!(computer.finished);
         assert_eq!(computer.pc, 4);
         assert_eq!(computer.memory, vec![1, 0, 4, 0, 99]);
         assert_eq!(computer.output, vec![1]);
+    }
+
+    #[test]
+    fn test_equal_position_mode() {
+        let trace = false;
+        let program_text = "3,9,8,9,10,9,4,9,99,-1,8";
+        let magic_number = 8;
+        assert_eq!(run_program(program_text, magic_number, trace), vec![1]);
+        assert_eq!(run_program(program_text, magic_number+1, trace), vec![0]);
+    }
+
+    #[test]
+    fn test_equal_immediate_mode() {
+        let trace = false;
+        let program_text = "3,3,1108,-1,8,3,4,3,99";
+        let magic_number = 8;
+        assert_eq!(run_program(program_text, magic_number, trace), vec![1]);
+        assert_eq!(run_program(program_text, magic_number+1, trace), vec![0]);
+    }
+
+    #[test]
+    fn test_less_than_position_mode() {
+        let trace = false;
+        let program_text = "3,9,7,9,10,9,4,9,99,-1,8";
+        let magic_number = 8;
+        let output = run_program(program_text, magic_number-1, trace);
+        assert_eq!(output, vec![1]);
+        let output = run_program(program_text, magic_number, trace);
+        assert_eq!(output, vec![0]);
+    }
+
+    #[test]
+    fn test_less_than_immediate_mode() {
+        let trace = false;
+        let program_text = "3,3,1107,-1,8,3,4,3,99";
+        let magic_number = 8;
+        let output = run_program(program_text, magic_number-1, trace);
+        assert_eq!(output, vec![1]);
+        let output = run_program(program_text, magic_number, trace);
+        assert_eq!(output, vec![0]);
+    }
+
+    #[test]
+    fn test_jump_position_mode() {
+        let trace = false;
+        let program_text = "3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9";
+        let magic_number = 0;
+        let output = run_program(program_text, magic_number, trace);
+        assert_eq!(output, vec![0]);
+        let output = run_program(program_text, magic_number+1, trace);
+        assert_eq!(output, vec![1]);
+    }
+
+    #[test]
+    fn test_jump_immediate_mode() {
+        let trace = false;
+        let program_text = "3,3,1105,-1,9,1101,0,0,12,4,12,99,1";
+        let magic_number = 0;
+        let output = run_program(program_text, magic_number, trace);
+        assert_eq!(output, vec![0]);
+        let output = run_program(program_text, magic_number+1, trace);
+        assert_eq!(output, vec![1]);
+    }
+
+    #[test]
+    fn test_larger_example() {
+        let trace = false;
+        let program_text = "3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99";
+        let magic_number = 8;
+        let output = run_program(program_text, magic_number - 1, trace);
+        assert_eq!(output, vec![999]);
+        let output = run_program(program_text, magic_number, trace);
+        assert_eq!(output, vec![1000]);
+        let output = run_program(program_text, magic_number + 1, trace);
+        assert_eq!(output, vec![1001]);
     }
 }
