9f093e7794f735536fc5cf26aaa49ceae30dca98
Matt Armstrong <matt@rfc20.org>
2023-11-12 14:52:13 -0800

    Day 20 part two

--- src/bin/20.rs
+++ src/bin/20.rs
@@ -11,7 +11,8 @@ type Point = Point2D<i32>;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord)]
 enum Position {
-    Label(char, char),
+    OuterLabel(char, char),
+    InnerLabel(char, char),
     Corridor(Point),
 }
 
@@ -71,13 +72,60 @@ impl Graph {
     }
 }
 
-fn measure_maze(graph: &Graph) -> u32 {
-    let start = Position::Label('A', 'A');
-    let successors = |pos: &Position| {
-        // println!("successors of {:?} => {:?}", pos, res);
-        graph.neighbors(pos)
+fn shortest_path_part_one(graph: &Graph) -> u32 {
+    let start = Position::OuterLabel('A', 'A');
+    let successors = |pos: &Position| graph.neighbors(pos);
+    let success = |pos: &Position| *pos == Position::OuterLabel('Z', 'Z');
+    if let Some((_path, distance)) = pathfinding::prelude::dijkstra(&start, successors, success) {
+        distance
+    } else {
+        unreachable!();
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
+struct PositionZ {
+    z: u32,
+    pos: Position,
+}
+
+impl PositionZ {
+    fn successor(&self, dest: &Position) -> Option<PositionZ> {
+        // println!("PositionZ::successor {:?} {:?}", self, dest);
+        let PositionZ { mut z, pos: source } = self;
+        match (*source, *dest) {
+            (Position::InnerLabel(_, _), Position::OuterLabel(_, _)) => {
+                z += 1;
+            }
+            (Position::OuterLabel(_, _), Position::InnerLabel(_, _)) => {
+                if z == 0 {
+                    return None;
+                }
+                z -= 1;
+            }
+            _ => {}
+        };
+        Some(PositionZ { z, pos: *dest })
+    }
+}
+
+fn shortest_path_part_two(graph: &Graph) -> u32 {
+    let start = PositionZ {
+        z: 0,
+        pos: Position::OuterLabel('A', 'A'),
     };
-    let success = |pos: &Position| *pos == Position::Label('Z', 'Z');
+    let end = PositionZ {
+        z: 0,
+        pos: Position::OuterLabel('Z', 'Z'),
+    };
+    let successors = |from_posz: &PositionZ| {
+        graph
+            .neighbors(&from_posz.pos)
+            .iter()
+            .filter_map(|(pos, weight)| from_posz.successor(pos).map(|succ| (succ, *weight)))
+            .collect::<Vec<_>>()
+    };
+    let success = |pos: &PositionZ| *pos == end;
     if let Some((_path, distance)) = pathfinding::prelude::dijkstra(&start, successors, success) {
         distance
     } else {
@@ -94,11 +142,19 @@ fn read_label(chars: &[Vec<char>], anchor: &Point, dir: CardinalDirection) -> Op
     let p1 = anchor.cardinal_neighbor(dir);
     let p2 = p1.cardinal_neighbor(dir);
     match (get(p1), get(p2)) {
-        (Some(ch1), Some(ch2)) if ch1.is_ascii_uppercase() && ch2.is_ascii_uppercase() => {
-            Some(match dir {
-                CardinalDirection::North | CardinalDirection::West => Position::Label(ch2, ch1),
-                CardinalDirection::South | CardinalDirection::East => Position::Label(ch1, ch2),
-            })
+        (Some(mut ch1), Some(mut ch2)) if ch1.is_ascii_uppercase() && ch2.is_ascii_uppercase() => {
+            match dir {
+                CardinalDirection::North | CardinalDirection::West => (ch1, ch2) = (ch2, ch1),
+                CardinalDirection::South | CardinalDirection::East => {}
+            }
+            let max_x: i32 = (chars[0].len() - 1).try_into().unwrap();
+            let max_y: i32 = (chars.len() - 1).try_into().unwrap();
+            let label = if p2.x == 0 || p2.x == max_x || p2.y == 0 || p2.y == max_y {
+                Position::OuterLabel(ch1, ch2)
+            } else {
+                Position::InnerLabel(ch1, ch2)
+            };
+            Some(label)
         }
         _ => None,
     }
@@ -113,6 +169,15 @@ fn parse_maze(input: &str) -> Graph {
         .map(|line| line.chars().collect::<Vec<char>>())
         .collect();
 
+    let find_attached_label = |point| {
+        for dir in CARDINAL_DIRECTIONS.iter() {
+            if let Some(label) = read_label(&chars, &point, *dir) {
+                return Some((*dir, label));
+            }
+        }
+        None
+    };
+
     for (y, line) in chars.iter().enumerate() {
         for (x, ch) in line.iter().enumerate() {
             let pos = Point::usize_new(x, y).unwrap();
@@ -122,21 +187,20 @@ fn parse_maze(input: &str) -> Graph {
                     let point = Point::usize_new(x, y).unwrap();
                     let corridor = Position::Corridor(point);
                     graph.add_node(corridor);
-                    for dir in CARDINAL_DIRECTIONS.iter() {
-                        if let Some(label) = read_label(&chars, &point, *dir) {
-                            match label {
-                                Position::Label('A', 'A') => {
-                                    graph.add_edge(label, corridor, 0);
-                                }
-                                Position::Label('Z', 'Z') => {
-                                    graph.add_edge(corridor, label, 0);
-                                }
-                                _ => {
-                                    graph.add_edge(corridor, label, 1);
-                                    graph.add_edge(label, corridor, 0);
-                                }
+                    if let Some((dir, label)) = find_attached_label(point) {
+                        graph.add_edge(label, corridor, 0);
+                        graph.add_edge(corridor, label, 0);
+                        match label {
+                            Position::OuterLabel(ch1, ch2) => {
+                                graph.add_edge(label, Position::InnerLabel(ch1, ch2), 1);
                             }
+                            Position::InnerLabel(ch1, ch2) => {
+                                graph.add_edge(label, Position::OuterLabel(ch1, ch2), 1);
+                            }
+                            _ => {}
                         }
+                    } else {
+                        graph.add_node(corridor);
                     }
                 }
                 ch if ch.is_ascii_uppercase() => {
@@ -174,21 +238,35 @@ mod test {
     #[test]
     fn test_part_one_small_example() {
         let graph = parse_maze(EXAMPLE_SMALL);
-        let distance = measure_maze(&graph);
+        let distance = shortest_path_part_one(&graph);
         assert_eq!(distance, 23);
     }
 
     #[test]
     fn test_part_one_example() {
         let graph = parse_maze(EXAMPLE);
-        let distance = measure_maze(&graph);
+        let distance = shortest_path_part_one(&graph);
         assert_eq!(distance, 58);
     }
 
     #[test]
     fn test_part_one() {
         let graph = parse_maze(INPUT);
-        let distance = measure_maze(&graph);
+        let distance = shortest_path_part_one(&graph);
         assert_eq!(distance, 510);
+    }
+
+    #[test]
+    fn test_part_two_small_example() {
+        let graph = parse_maze(EXAMPLE_SMALL);
+        let distance = shortest_path_part_two(&graph);
+        assert_eq!(distance, 26);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let graph = parse_maze(INPUT);
+        let distance = shortest_path_part_two(&graph);
+        assert_eq!(distance, 5652);
     }
 }
